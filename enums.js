af31cdd38f186c36ba71fbd5296eb05ed5e9ff74
CGuillotG <carlos@guillot.dev>
2024-05-19 18:41:52 -0600

    Modified Old Notes Enum

--- enums.js
+++ enums.js
@@ -2,7 +2,7 @@ const guardians = ['Titan', 'Warlock', 'Hunter']
 const fields = ['Mobility (Base)', 'Resilience (Base)', 'Recovery (Base)', 'Discipline (Base)', 'Intellect (Base)', 'Strength (Base)']
 const totalField = 'Total (Base)'
 const slots = ['Helmet', 'Gauntlets', 'Chest Armor', 'Leg Armor']
-const oldNotes = ['AFK', 'TEMP', 'EXOTIC', 'MASTERWORK', 'ARTIFICE', 'IB', 'DELETE']
+const oldNotes = ['AFK', 'TEMP', 'EXOTIC', 'IB', 'Artifice', 'Trials', 'LastWish']
 
 const fieldMap = {
   'Mob': 'Mobility (Base)',
