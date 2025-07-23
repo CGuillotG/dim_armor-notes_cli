bef31b2ebbda98a7cfd2e7e549591c051b6106cf
CGuillotG <carlos@guillot.dev>
2025-07-23 16:59:27 -0600

    Remove extraArmorData.js from tracking

--- extraArmorData.js
+++
@@ -1,8 +0,0 @@
-// Extra Armor formatted like this:
-// [NAME, SLOT, CLASS, mob, res, rec, dis, int, str, total]
-// Artifice is detected from name containing "(Artifice)"
-// Exotic is detected from name containing "EXOTIC"
-
-export const extraArmorData = [
-  // Add armor entries here following the format above
-] 
